f12b87ffa3e2cded6b4b53a69c1a1670e9804e5c
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2014-05-23 19:42:28 +0430

    fix
    fix flyout width
    make edit flyout input fonts a little larger

---
+++ Wordbook/Converters/SizeRatioConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Wordbook.Converters
+{
+    public class SizeRatioConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var ratio = System.Convert.ToDouble(parameter);
+
+            if (Math.Abs(ratio) <= 0)
+            {
+                ratio = 1d;
+            }
+
+            var size = System.Convert.ToDouble(value);
+
+            return size * ratio;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
